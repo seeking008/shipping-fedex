0a43a74bce697190edb6fac3f4e44a52f23b0238
Michael <essentialmichael@gmail.com>
2025-07-01 17:39:45 -0700

    Add STATIC_ROOT to settings.py

--- fedex_project/settings.py
+++ fedex_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +119,8 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
