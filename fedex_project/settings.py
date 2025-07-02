307eb92b26e143b85a3366d49deae0a54c8ce93c
Michael <essentialmichael@gmail.com>
2025-07-01 18:02:09 -0700

    Connect views and set Home as homepage

--- fedex_project/settings.py
+++ fedex_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0_(a)%=@o&3&q#$mu^o7utmrirw2&*4w74+7t#^&lpxdc%z$mw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fedex-project-2.onrender.com']
 
